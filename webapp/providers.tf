5ffd20e737a914f996f7e3ce202ffd9594d4b019
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-05-30 21:28:38 +0530

    Update providers.tf

--- webapp/providers.tf
+++ webapp/providers.tf
@@ -10,9 +10,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "ca907e21-0e0d-4865-b86b-76b747e9fc86"
-  tenant_id       = "665b6c62-7310-4d39-9abb-32a0cbc3b90f"
-  client_secret   = "1nc8Q~zCNLGTEfmklHur3FjMkeCszQI-_hQurbvV"
-  subscription_id = "727569fb-5294-4ca5-9005-466abf5d33f1"
+
 
 }
