a8b8861fe80d228d1918f326720d1defa5d462a3
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-05-30 20:59:26 +0530

    Update main.tf

--- key-vault/main.tf
+++ key-vault/main.tf
@@ -1,38 +1,23 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.18.0"
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
     }
   }
 }
 
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-    }
-
-  client_id       = "ca907e21-0e0d-4865-b86b-76b747e9fc86"
-  tenant_id       = "665b6c62-7310-4d39-9abb-32a0cbc3b90f"
-  client_secret   = "1nc8Q~zCNLGTEfmklHur3FjMkeCszQI-_hQurbvV"
-  subscription_id = "727569fb-5294-4ca5-9005-466abf5d33f1"
-
-}
-
-
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "r1" {
+resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_key_vault" "kv" {
-  name                        = "keyvault01"
-  location                    = azurerm_resource_group.r1.location
-  resource_group_name         = azurerm_resource_group.r1.name
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
@@ -50,27 +35,10 @@ resource "azurerm_key_vault" "kv" {
 
     secret_permissions = [
       "Get",
-      "list",
-      "set",
-      "delete",
     ]
 
     storage_permissions = [
       "Get",
     ]
   }
-}
-
-resource "azurerm_key_vault_access_policy" "policy" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = "625bd1ca-3e34-4697-9c65-91080f3ab368" # SPN ID
-
-  key_permissions = [
-    "get",
-  ]
-
-  secret_permissions = [
-    "get",
-  ]
 }
