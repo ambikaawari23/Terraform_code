48c06af004f0ae8bf645e308c49cd9f8e93025b6
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-06-01 19:33:53 +0530

    Update main.tf

--- adf/main.tf
+++ adf/main.tf
@@ -1,10 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.18.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
 resource "azurerm_resource_group" "rg" {
   name     = "adf-resources"
   location = "West Europe"
 }
 
 resource "azurerm_data_factory" "adf01" {
-  name                = "aa_adf"
+  name                = "aaadf23"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
